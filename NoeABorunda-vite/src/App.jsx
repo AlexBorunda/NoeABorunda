baf57ffc6abde76433b66b667b9acf6a0f3b682d
aborunda <alex.borunda01@gmail.com>
2024-12-14 16:50:51 -0700

    Inlcuded a profile pricture by integrating the header and image into a div and formated it. Additionally, Ive linked Colorado school of mines, github, and linkedin profiles to the website.

--- NoeABorunda-vite/src/App.jsx
+++ NoeABorunda-vite/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import linkedinLogo from './assets/linkedin-white.png'
+import coloradoschoolofminesLogo from './assets/coloradoschoolofmines-white.png'
+import githubLogo from './assets/github-white.png'
+import profilePicture from './assets/profile.jpg'
 import './App.css'
 
 function App() {
@@ -9,22 +11,44 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        {/* Link to Colorado School of Mines website w/ Mines Legacy logo */}
+        <a href="https://www.mines.edu/" target="_blank">
+          <img src={coloradoschoolofminesLogo} className="logo" alt="Colorado school of mines legacy logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+
+        {/* Link to Noe Borunda's Linkedin page website w/ Linkedin logo */}
+        <a href="https://www.linkedin.com/in/noeaborunda/" target="_blank">
+          <img src={linkedinLogo} className="logo react" alt="Linkedin logo" />
+        </a>
+
+        {/* Link to Noe Borunda's Github page website w/ Github logo */}
+        <a href="https://github.com/aborunda/" target="_blank">
+          <img src={githubLogo} className="logo vite" alt="Github logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
+      <div className="profile-header">
+        {/* Profile picture */}
+        <img src={profilePicture} className="profile-picture" alt="Noe's profile picture" />        
+
+        {/* Title of the website*/}
+        <h1>Noe A Borunda</h1>
+      </div>
+
+      {/* Card with the count */}
       <div className="card">
+        {/* Button to increment the count */}
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+          
+        {/* Instruction to edit the App.jsx file*/}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
+      {/* Paragraph with the class 'read-the-docs' */}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
