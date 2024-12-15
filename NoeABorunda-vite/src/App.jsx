78e0dd3518a0399b40433f0117521c0a9a04535e
aborunda <alex.borunda01@gmail.com>
2024-12-15 12:36:14 -0700

    I reformatted the ordering of the github, linkedin, & Mines icons by containerizing the image and logo. Along w/ the profile picture header title and header subtitle.

--- NoeABorunda-vite/src/App.jsx
+++ NoeABorunda-vite/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+{/* import { useState } from 'react' */}
 import linkedinLogo from './assets/linkedin-white.png'
 import coloradoschoolofminesLogo from './assets/coloradoschoolofmines-white.png'
 import githubLogo from './assets/github-white.png'
@@ -6,51 +6,77 @@ import profilePicture from './assets/profile.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  {/* const [count, setCount] = useState(0) */}
 
   return (
     <>
-      <div>
-        {/* Link to Colorado School of Mines website w/ Mines Legacy logo */}
-        <a href="https://www.mines.edu/" target="_blank">
-          <img src={coloradoschoolofminesLogo} className="logo" alt="Colorado school of mines legacy logo" />
-        </a>
-
-        {/* Link to Noe Borunda's Linkedin page website w/ Linkedin logo */}
-        <a href="https://www.linkedin.com/in/noeaborunda/" target="_blank">
-          <img src={linkedinLogo} className="logo react" alt="Linkedin logo" />
-        </a>
-
-        {/* Link to Noe Borunda's Github page website w/ Github logo */}
-        <a href="https://github.com/aborunda/" target="_blank">
-          <img src={githubLogo} className="logo vite" alt="Github logo" />
-        </a>
-      </div>
-
+      
       <div className="profile-header">
         {/* Profile picture */}
-        <img src={profilePicture} className="profile-picture" alt="Noe's profile picture" />        
+        <img src={profilePicture} className="profile-picture" alt="Noe's profile picture" />     
 
-        {/* Title of the website*/}
-        <h1>Noe A Borunda</h1>
+        <div className="profile-header-text">
+          {/* Title of the website*/}
+          <h1>Noe (Alex) Borunda</h1>
+
+          {/* Subtitle of the website */}
+          <h2> CS Undergraduate at {' '}
+            <a href= "https://www.mines.edu/" target="_blank" rel="noopener noreferrer">
+              Colorado School of Mines
+            </a>
+          </h2>
+        </div>
+
       </div>
 
+      <div className="logos-container">
+        
+        {/* Link to Noe Borunda's Github page website w/ Github logo */}
+        <div className="logos-container-item">
+          <a href="https://github.com/aborunda/" target="_blank">
+            <img src={githubLogo} className="logo vite" alt="Github logo" />
+          </a>
+          <h2>Github</h2>
+        </div>
+
+        {/* Link to Noe Borunda's Linkedin page website w/ Linkedin logo */}
+        <div className="logos-container-item">
+          <a href="https://www.linkedin.com/in/noeaborunda/" target="_blank">
+            <img src={linkedinLogo} className="logo react" alt="Linkedin logo" />
+          </a>
+          <h2>Linkedin</h2>
+        </div>
+
+        {/* Link to Colorado school of mines website w/ mines legacy logo */}
+        <div className="logos-container-item">
+          <a href="https://cs.mines.edu/" target="_blank">
+            <img src={coloradoschoolofminesLogo} className="logo mines" alt="Colorado school of mines legacy logo" />
+          </a>
+          <h2>CS @ Mines</h2>
+        </div>
+
+      </div>
+
+      
       {/* Card with the count */}
+      {/*
       <div className="card">
-        {/* Button to increment the count */}
+        Button to increment the count
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
           
-        {/* Instruction to edit the App.jsx file*/}
+        Instruction to edit the App.jsx file
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      */}
+      
 
       {/* Paragraph with the class 'read-the-docs' */}
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Github, Linkedin, & Mines logos to learn more
       </p>
     </>
   )
